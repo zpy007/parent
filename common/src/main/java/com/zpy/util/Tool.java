df5fe52da7e2303d30f0418ab09a2481d5560900
zpy007 <zpy2358@gmail.com>
2018-05-27 16:05:28 +0800

    20180527

--- common/src/main/java/com/zpy/util/Tool.java
+++ common/src/main/java/com/zpy/util/Tool.java
@@ -1,7 +1,17 @@
 package com.zpy.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Tool {
+    public Hand hand;
+
+    @Autowired
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
     public void Use(){
         System.out.println("Use Tool");
+        hand.eat();
     }
 }
--- manager/src/main/java/com/zpy/core/Execute.java
+++ manager/src/main/java/com/zpy/core/Execute.java
@@ -5,12 +5,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Execute {
+
     public static void main(String[] args){
 
         ApplicationContext context = new ClassPathXmlApplicationContext("content.xml");
-        ZBeans beans=context.getBean("myexe",ZBeans.class);
-        beans.Fun();
-        Tool tool=new Tool();
+        Tool tool=context.getBean("tool",Tool.class);
         tool.Use();
+
     }
 }
